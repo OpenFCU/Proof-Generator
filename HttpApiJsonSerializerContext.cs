56a13f4cd7d78d90ff05aeffed855bc6df07920a
mikucat0309 <admin@mikuc.at>
2023-07-08 12:30:25 +0800

    fix: no JsonTypeInfo registered

--- HttpApiJsonSerializerContext.cs
+++ HttpApiJsonSerializerContext.cs
@@ -4,6 +4,7 @@ using Amazon.Lambda.APIGatewayEvents;
 namespace ProofGenerator;
 
 [JsonSerializable(typeof(Request))]
+[JsonSerializable(typeof(Response))]
 [JsonSerializable(typeof(APIGatewayHttpApiV2ProxyRequest))]
 [JsonSerializable(typeof(APIGatewayHttpApiV2ProxyResponse))]
 public partial class HttpApiJsonSerializerContext : JsonSerializerContext
